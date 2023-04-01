48a739e9f0f14aa58e5e041bffd411c1c4ac30d3
Tudor Butufei <tudor.butufei@gmail.com>
2023-04-01 16:35:27 +0300

    add firebase support

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(MyApp());
+Future<void> main() async {
+  // initialize widgets
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // initialize firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // run application
+  runApp(const MyApp()); 
 }
 
 class MyApp extends StatelessWidget {
