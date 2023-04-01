03823f8119927e22cb8cfa20b4151eadd91f9c94
Bucovina <103101131+Bucovina@users.noreply.github.com>
2023-04-02 00:43:20 +0300

    generate QR code based on user id

--- lib/pages/qr.dart
+++ lib/pages/qr.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class QR extends StatelessWidget {
   const QR({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
+    final user = FirebaseAuth.instance.currentUser!;
     return Center(
-      child: Image.asset(
-        'assets/images/QR_code.png',
+      child: Stack(
+        alignment: const Alignment(0, -0.17),
+        children: [
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20),
+              child: Image.asset(
+            'assets/images/shield.png',
+          )),
+          QrImage(
+            data: user.uid,
+            size: 225,
+          )
+        ],
       ),
     );
   }
