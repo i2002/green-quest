054eda232d58493312c04312c008e70e051b5a95
Bucovina <103101131+Bucovina@users.noreply.github.com>
2023-04-01 16:38:38 +0300

    add app navigation

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,56 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      ///debug banner off
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.green,
-          title: Text('Green Quest'),
+          title: const Text('Green Quest'),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              print('Ana are mere!');
+            },
+            child: const Text('Click'),
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              label: 'Home',
+              icon: Icon(Icons.home),
+            ),
+            BottomNavigationBarItem(
+              //!Good name for QR
+              label: 'QR',
+              icon: Icon(Icons.qr_code_rounded),
+            ),
+            BottomNavigationBarItem(
+              label: 'Settings',
+              icon: Icon(Icons.settings),
+            ),
+          ],
+          currentIndex: currentIndex,
+          onTap: (int index) {
+            setState(() {
+              currentIndex = index;
+            });
+          },
         ),
         backgroundColor: Colors.greenAccent,
       ),
