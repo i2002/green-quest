a8dedc0ad0ef43be7058efe9bc240b64a404d04d
Bucovina <103101131+Bucovina@users.noreply.github.com>
2023-04-01 15:16:11 +0300

    Demo

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: Text('Green Quest'),
+        ),
+        backgroundColor: Colors.greenAccent,
+      ),
+    );
+  }
+}
