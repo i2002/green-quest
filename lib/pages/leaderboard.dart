64682cc34a22e21c1cc3ae41f27a925a451bdc77
Tudor Butufei <tudor.butufei@gmail.com>
2023-04-02 10:53:20 +0300

    add leader board

--- lib/pages/leaderboard.dart
+++ lib/pages/leaderboard.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class Leaderboard extends StatelessWidget {
@@ -5,6 +6,44 @@ class Leaderboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(children: [
+      StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+          .collection("/user-profiles")
+          .orderBy("xp", descending: true)
+          .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          }
+
+          final users = snapshot.data?.docs;
+
+          if (users!.isEmpty) {
+            return const Expanded(child:
+              Card(
+                child: Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Text("No quests to show.")
+                )
+              )
+            );
+          }
+
+          return Expanded(child: ListView.builder(
+            shrinkWrap: true,
+            itemCount: users.length,
+            itemBuilder: (context, index) {
+              return Card(
+                child: ListTile(
+                  title: Text(users[index]["name"]),
+                  subtitle: Text("XP: ${users[index]["xp"]}")
+                ),
+              );
+            },
+          ));
+        }
+      )
+    ],);
   }
 }
