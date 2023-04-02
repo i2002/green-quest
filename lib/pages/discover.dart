6fd7f1f53c881a30ffb939b54b53cdf8a3f0fe99
Tudor Butufei <tudor.butufei@gmail.com>
2023-04-02 08:20:16 +0300

    permit adding and aborting quests

--- lib/pages/discover.dart
+++ lib/pages/discover.dart
@@ -1,13 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class Quest {
+  final String id;
   final String name;
   final String details;
   final DateTime date;
 
-  const Quest(this.name, this.details, this.date);
+  const Quest(this.id, this.name, this.details, this.date);
 }
 
 class Discover extends StatefulWidget {
@@ -23,6 +25,8 @@ class _DiscoverState extends State<Discover> {
     return StreamBuilder<QuerySnapshot>(
       stream: FirebaseFirestore.instance
           .collection("/quests")
+          .orderBy("date")
+          .where("date", isGreaterThanOrEqualTo: Timestamp.fromDate(DateTime.now()))
           .snapshots(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
@@ -38,14 +42,21 @@ class _DiscoverState extends State<Discover> {
         return ListView.builder(
           itemCount: quests.length,
           itemBuilder: (context, index) {
-            Quest q = Quest(quests[index]["name"], quests[index]["details"],
-                quests[index]["date"].toDate());
+            var questData = quests[index];
+            Quest q = Quest(
+              questData.id, 
+              questData["name"], 
+              questData["details"],
+              questData["date"].toDate()
+            );
+
             String formattedDate = DateFormat.yMMMMd()
                 .add_jm()
-                .format(quests[index]["date"].toDate());
+                .format(questData["date"].toDate());
+
             return Card(
               child: ListTile(
-                title: Text(quests[index]["name"]),
+                title: Text(questData["name"]),
                 subtitle: Text("Date: $formattedDate"),
                 onTap: () {
                   Navigator.push(
@@ -77,52 +88,99 @@ class QuestScreen extends StatelessWidget {
         title: const Text("Quest details"),
         backgroundColor: Colors.greenAccent,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
+      body: StreamBuilder<bool>(
+        stream: FirebaseFirestore.instance
+          .collection("/quests/${quest.id}/participants")
+          .snapshots()
+          .asyncMap((snapshot) {
+            final user = FirebaseAuth.instance.currentUser!;
+            final attending = snapshot.docs.where((element) => element.id == user.uid).isNotEmpty;
+            return attending;
+          }),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          }
+          final attending = snapshot.data;
+
+          if(attending == null) {
+            return const Text("Data error");
+          }
+
+          return Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
               children: [
-                Flexible(child:
-                  Container(
-                    margin: const EdgeInsets.only(bottom: 10),
-                    child: Text(quest.name, style: Theme.of(context).textTheme.displaySmall)
-                  )
-                )
-            ]),
-            Row(
-              children: [
-                Flexible(child: Text(
-                  quest.details,
-                  textScaleFactor: 1.5,
-                ))
-              ],
-            ),
-            Expanded(child:
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  SizedBox(
-                    width: 200,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: const Text('Take the Quest!'),
-                    ),
+                Row(
+                  children: [
+                    Flexible(child:
+                      Container(
+                        margin: const EdgeInsets.only(bottom: 10),
+                        child: Text(quest.name, style: Theme.of(context).textTheme.displaySmall)
+                      )
+                    )
+                ]),
+                Row(
+                  children: [
+                    Flexible(child: Text(
+                      quest.details,
+                      textScaleFactor: 1.5,
+                    ))
+                  ],
+                ),
+                Expanded(child:
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      SizedBox(
+                        width: 200,
+                        child: ElevatedButton(
+                          onPressed: () => attending ? abortQuest(quest.id) : attendQuest(quest.id),
+                          child: Text((() => attending ? 'Abort quest' : 'Take the Quest!')()),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Container(margin: const EdgeInsets.only(top: 10),child: Text('Date: $formattedDate'),),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(top: 10),
+                      child: Text('Date: $formattedDate')
+                    ),
+                  ],
+                ),
               ],
             ),
-          ],
-        ),
-      ),
+          );
+        }
+      )
     );
+  }
+
+  Future attendQuest(questId) async {
+    var db = FirebaseFirestore.instance;
+    final user = FirebaseAuth.instance.currentUser!;
+    var attendingRef = db.doc("/user-profiles/${user.uid}/quests/$questId");
+    var attendeeRef = db.doc("/quests/$questId/participants/${user.uid}");
+
+    var batch = db.batch();
+    batch.set(attendingRef, {"confirmed": false});
+    batch.set(attendeeRef, {"confirmed": false});
+    await batch.commit();
+  }
+
+  Future abortQuest(questId) async {
+    var db = FirebaseFirestore.instance;
+    final user = FirebaseAuth.instance.currentUser!;
+    var attendingRef = db.doc("/user-profiles/${user.uid}/quests/$questId");
+    var attendeeRef = db.doc("/quests/$questId/participants/${user.uid}");
+
+    var batch = db.batch();
+    batch.delete(attendingRef);
+    batch.delete(attendeeRef);
+    await batch.commit();
   }
 }
