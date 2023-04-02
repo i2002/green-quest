2ae2737512225332ca1f2c75546e07db23a48b11
Tudor Butufei <tudor.butufei@gmail.com>
2023-04-02 03:06:40 +0300

    add discover page

--- lib/pages/discover.dart
+++ lib/pages/discover.dart
@@ -1,4 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class Quest {
+  final String name;
+  final String details;
+  final DateTime date;
+
+  const Quest(this.name, this.details, this.date);
+}
 
 class Discover extends StatefulWidget {
   const Discover({super.key});
@@ -10,8 +20,38 @@ class Discover extends StatefulWidget {
 class _DiscoverState extends State<Discover> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Text('discover working'),
+    return StreamBuilder<QuerySnapshot>(stream: FirebaseFirestore.instance.collection("/guilds/TrFm0hrtDXiP9VwfYltu/quests").snapshots(), builder: (context, snapshot) {
+      if (snapshot.connectionState == ConnectionState.waiting) {
+        return const CircularProgressIndicator();
+      }
+
+      final quests = snapshot.data?.docs;
+
+      if (quests!.isEmpty) {
+        return const Text("No data.");
+      }
+
+      return ListView.builder(itemCount: quests.length, itemBuilder: (context, index) {
+        Quest q = Quest(quests[index]["name"], quests[index]["details"], quests[index]["date"].toDate());
+        String formattedDate = DateFormat.yMMMMd().add_jm().format(quests[index]["date"].toDate());
+        return Card(child: ListTile(title: Text(quests[index]["name"]), subtitle: Text("Date: $formattedDate"), onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => QuestScreen(quest: q),));
+        },));
+      });
+    });
+  }
+}
+
+class QuestScreen extends StatelessWidget {
+  const QuestScreen({super.key, required this.quest});
+
+  final Quest quest;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(quest.name)),
+      body: Padding(padding: const EdgeInsets.all(16.0), child: Text(quest.details)),
     );
   }
 }
